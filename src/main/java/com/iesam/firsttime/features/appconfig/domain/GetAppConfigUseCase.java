544a083c4d55718dfd9caa496b27a741c6a4d54c
Alessandro Paucar <146753377+paucare@users.noreply.github.com>
2024-03-07 17:06:01 +0100

    Commit #3

---
+++ src/main/java/com/iesam/firsttime/features/appconfig/domain/GetAppConfigUseCase.java
@@ -0,0 +1,5 @@
+package com.iesam.firsttime.features.appconfig.domain;
+
+public class GetAppConfigUseCase {
+
+}
--- src/test/java/com/iesam/firsttime/features/vote/domain/VoteAppUseCaseTest.java
+++ src/test/java/com/iesam/firsttime/features/vote/domain/VoteAppUseCaseTest.java
@@ -1,11 +1,15 @@
 package com.iesam.firsttime.features.vote.domain;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class VoteAppUseCaseTest {
+
+    VoteAppUseCase voteAppUseCase = null;
 
     @BeforeEach
     void setUp() {
@@ -13,5 +17,32 @@ class VoteAppUseCaseTest {
 
     @AfterEach
     void tearDown() {
+         voteAppUseCase = null;
+
+    }
+    @Test
+    public void cuanndoAppConfigEsNuloEntoncesMostrarVotarEsFalse(){
+
+        //Given/Dado
+        voteAppUseCase = new VoteAppUseCase(new StubNullAppConfigDataRepository));
+
+        //When
+        Boolean result = voteAppUseCase.execute();
+
+        //Then/Entonces
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void cuandoLaAppSeHaAbiertoDistintoDeCincoVecesEntoncesMostrarVotarEsFalse(){
+
+        //Given
+        voteAppUseCase = new VoteAppUseCase(new StubNot5AppConfigDataRepository());
+
+        //When
+        Boolean result= voteAppUseCase.execute();
+
+        //Then
+        Assertions.assertFalse(result);
     }
 }
